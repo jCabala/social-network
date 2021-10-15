7bdba0dbb67e27219e395b2a3c47d1bb8df6523c
Jakub Cabala <kuba.cabala6@gmail.com>
2021-10-15 16:53:00 +0200

    Finishing backend

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -175,34 +175,6 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
-//@route    GET api/profile/user/:user_id
-//@desc     Get profile by user user_id
-//@access   Public
-
-router.get('/user/:user_id', async (req, res) => {
-  try {
-    const profile = await Profile.findOne({
-      user: req.params.user_id,
-    }).populate('user', ['name', 'avatar']);
-
-    if (!profile)
-      return res
-        .status(400)
-        .json({ msg: 'There is no profile for this user ' });
-
-    res.json(profile);
-  } catch (err) {
-    console.error(err.message);
-
-    if (err.kind == 'ObjectId') {
-      return res
-        .status(400)
-        .json({ msg: 'There is no profile for this user ' });
-    }
-    res.status(500).send('Server error');
-  }
-});
-
 //@route    DELETE api/profile
 //@desc     Delete profile, user & posts
 //@access   Private
